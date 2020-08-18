4a61aed4c014e8d96075d7cd0d81877a20beecd5
Anh Mac <anhmv91@gmail.com>
2020-08-18 23:53:17 +0700

    correct spacing

--- Sources/Amlich/Amlich.swift
+++ Sources/Amlich/Amlich.swift
@@ -104,16 +104,16 @@ struct Amlich {
      */
     static func sunLongtitude(of jdn: Double) -> Double {
         let T: Double = (jdn - 2451545.0 ) / 36525 // Time in Julian centuries from 2000-01-01 12:00:00 GMT
-        let T2: Double = T*T
+        let T2: Double = T * T
         let dr: Double = Double.pi / 180 // degree to radian
-        let M: Double = 357.52910 + 35999.05030*T - 0.0001559*T2 - 0.00000048*T*T2 // mean anomaly, degree
-        let L0: Double = 280.46645 + 36000.76983*T + 0.0003032*T2 // mean longitude, degree
-        var DL: Double = (1.914600 - 0.004817*T - 0.000014*T2)*sin(dr*M)
-        DL = DL + (0.019993 - 0.000101*T)*sin(dr*2*M) + 0.000290*sin(dr*3*M)
+        let M: Double = 357.52910 + 35999.05030 * T - 0.0001559 * T2 - 0.00000048 * T * T2 // mean anomaly, degree
+        let L0: Double = 280.46645 + 36000.76983 * T + 0.0003032 * T2 // mean longitude, degree
+        var DL: Double = (1.914600 - 0.004817 * T - 0.000014 * T2) * sin(dr * M)
+        DL = DL + (0.019993 - 0.000101 * T) * sin(dr * 2 * M) + 0.000290 * sin(dr * 3 * M)
+
         var L: Double = L0 + DL // true longitude, degree
-        L = L*dr
-        
-        L = L - Double.pi * 2 * Double(Int(floor(L/(Double.pi * 2)))) // Normalize to (0, 2*PI)
+        L = L * dr
+        L = L - Double.pi * 2 * Double(Int(floor(L / (Double.pi * 2)))) // Normalize to (0, 2*PI)
         return L
     }
 
